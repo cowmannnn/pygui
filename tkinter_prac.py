b4b47ae0ae10fb2848cb37a4c08274d76de0c4e5
alexconecko <89869524+alexconecko@users.noreply.github.com>
2021-11-11 18:10:58 +1100

    Update tkinter_prac.py

--- tkinter_prac.py
+++ tkinter_prac.py
@@ -5,22 +5,23 @@ from tkinter import *
 # so that we can access its methods
 root = Tk()
 
-#e used as reference variable for entry method that 
-#displays a text box for user input
+# e used as reference variable for entry method that
+# displays a text box for user input
 e = Entry(root, width=50, bg="pink")
 e.grid(row=0, column=0)
 
-#function defined to run code when button is clicked
+
+# function defined to run code when button is clicked
 def aClick():
-    label = Label(root, text="my name jeff")
-    label.grid(row=0, column=0)
+    label = Label(root, text=e.get())
+    label.grid(row=1, column=0)
 
 
 # call a function with the button using command parameter,
 # padding functions similarly to html padding
-button1 = Button(root, text="click me!", padx=50, pady=10, command=aClick)
-
+button1 = Button(root, text="enter your name",
+                 padx=50, pady=10, command=aClick)
 button1.grid(row=1, column=0)
 
-
+# event loop
 root.mainloop()
