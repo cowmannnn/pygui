35c4b8ce4a46c3dbb0a6e135063cace8d899c6f4
alexconecko <89869524+alexconecko@users.noreply.github.com>
2021-11-14 16:24:32 +1100

    Update lifestyleAppDemo.py

--- lifestyleAppDemo.py
+++ lifestyleAppDemo.py
@@ -7,11 +7,14 @@ from PIL import ImageTk, Image
 # creates a reference variable to the Tkinter library
 # so that we can access its methods
 root = Tk()
-#
+# creates a reference variable to the Style methods
 style = ttk.Style()
+# window size is set
 root.geometry("350x500")
+# title of app changed
 root.title("WorkLife App v0.1")
-#
+
+# class created for the main window
 class Window():
     mainframe = ttk.Frame(root, padding="3 3 12 12")
     mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
